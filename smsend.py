0da5c2b0645081e98a6dd34b1cb4c306b6f1d3fe
hModule =/ <60050784+x86senniral@users.noreply.github.com>
2025-01-13 07:05:13 -0800

    Update smsend.py

--- smsend.py
+++ smsend.py
@@ -39,7 +39,7 @@ def Intro():
          {Colors.BLUE} By Sytaxus / cmdSNR {Colors.RESET}
     Instructions: This tool may contain some {Colors.RED} limitations{Colors.RESET}.
     - In regards to questions, open an issue in the github repo
-    - Check the https://github.com/sytaxus/SMSend-Anon-SMS-Sender/ , for full documentation if stuck.
+    - Check the https://github.com/x86senniral/SMSend-Anon-SMS-Sender/ , for full documentation if stuck.
 """)
     
 def cls_scrn():
